bd3298132af7c01735be144fc604a88bff4fc12e
KietNGer <101463615+KietNGer@users.noreply.github.com>
2022-06-08 22:23:09 +0700

    Add files via upload

---
+++ bai_1.cpp
@@ -0,0 +1,260 @@
+#include<iostream>
+#include<string.h>
+#include<math.h>
+#include<iomanip>
+#include<algorithm>
+using namespace std;
+typedef long long ll;
+
+class NhanVien
+{
+protected:
+    string HoTen;
+    int ngay;
+    int thang;
+    int nam;
+    long Luong;
+public:
+    NhanVien();
+    ~NhanVien();
+    virtual void Nhap();
+    virtual void Xuat();
+    bool kiemtra();
+    virtual long Tinh_luong();
+};
+
+class NVVP: public NhanVien
+{
+    int songaylamviec;
+public:
+    NVVP();
+    ~NVVP();
+    void Nhap();
+    void Xuat();
+    long Tinh_luong();
+};
+
+class NVSX: public NhanVien
+{
+    int sosanpham;
+    int Luongcoban;
+public:
+    NVSX();
+    ~NVSX();
+    void Nhap();
+    void Xuat();
+    long Tinh_luong();
+};
+
+//NhanVien
+NhanVien ::NhanVien()
+{
+    HoTen = "";
+    ngay = 0;
+    thang = 0;
+    nam = 0;
+    Luong = 0;
+}
+
+NhanVien ::~NhanVien()
+{
+    HoTen = "";
+    ngay = 0;
+    thang = 0;
+    nam = 0;
+    Luong = 0;
+}
+
+bool NhanVien ::kiemtra()
+{
+    if(nam < 0 || thang <= 0 || thang > 12)
+        return false;
+    if(nam % 4 == 0 && nam % 100 != 0 || nam % 400 == 0)
+    {
+        switch(thang)
+        {
+        case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+            if(ngay <= 0 || ngay > 31)
+                return false;
+            break;
+        case 4: case 6: case 9: case 11:
+            if(ngay <= 0 || ngay > 30)
+                return false;
+            break;
+        case 2:
+            if(ngay <= 0 || ngay > 29)
+                return false;
+            break;
+        }
+    }
+
+    else
+    {
+        switch(thang)
+        {
+        case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+            if(ngay <= 0 || ngay > 31)
+                return false;
+            break;
+        case 4: case 6: case 9: case 11:
+            if(ngay <= 0 || ngay > 30)
+                return false;
+            break;
+        case 2:
+            if(ngay <= 0 || ngay > 28)
+                return false;
+            break;
+        }
+    }
+
+    return true;
+}
+
+void NhanVien ::Nhap()
+{
+    cout << "Nhap thong tin nhan vien vao: \n";
+    cout << "Nhap ten nhan vien: ";
+    cin.ignore();
+    getline(cin, HoTen);
+    do
+    {
+        cout << "Nhap ngay thang nam sinh cua nhan vien: ";
+        cin >> ngay >> thang >> nam;
+    }while(kiemtra() == false);
+}
+
+void NhanVien ::Xuat()
+{
+    cout << "THONG TIN CUA NHAN VIEN\n";
+    cout << "+Ten Nhan vien: " << HoTen << endl;
+    cout << "+Ngay thang nam: " << ngay << "/" << thang << "/" << nam << endl;
+}
+
+long NhanVien ::Tinh_luong()
+{
+    return Luong;
+}
+
+//NVVP
+NVVP ::NVVP()
+{
+    songaylamviec = 0;
+}
+
+NVVP ::~NVVP()
+{
+    songaylamviec = 0;
+}
+
+void NVVP::Nhap()
+{
+    NhanVien ::Nhap();
+    cout << "Nhap so ngay lam viec cua nhan vien: ";
+    cin >> songaylamviec;
+}
+
+void NVVP ::Xuat()
+{
+    NhanVien ::Xuat();
+    cout << "+So ngay lam viec cua nhan vien: " << songaylamviec << endl;
+    cout << "+Luong cua nhan vien van phong la: " << Tinh_luong();
+    cout << endl;
+}
+
+long NVVP ::Tinh_luong()
+{
+    Luong = songaylamviec * 100000;
+    return Luong;
+}
+
+//NVSX
+NVSX ::NVSX()
+{
+    Luongcoban = 0;
+    sosanpham = 0;
+}
+
+NVSX ::~NVSX()
+{
+    Luongcoban = 0;
+    sosanpham = 0;
+}
+
+void NVSX ::Nhap()
+{
+    NhanVien ::Nhap();
+    cout << "Nhap luong co ban cua nhan vien: ";
+    cin >> Luongcoban;
+    cout << "Nhap so san pham nhan vien lam duoc: ";
+    cin >> sosanpham;
+}
+
+void NVSX ::Xuat()
+{
+    NhanVien ::Xuat();
+    cout << "+Luong co ban cua nhan vien: " << Luongcoban << endl;
+    cout << "+So san pham nhan vien lam duoc: " << sosanpham << endl;
+    cout << "+Luong cua nhan vien: " << Tinh_luong();
+    cout << endl;
+}
+
+long NVSX ::Tinh_luong()
+{
+    Luong = Luongcoban + sosanpham * 5000;
+    return Luong;
+}
+
+int main()
+{
+    cout << "Nhap so luong nhan vien van phong: ";
+    int n1;
+    cin >> n1;
+    NhanVien *x[n1];
+    for(int i = 0; i < n1; i++)
+    {
+        int n;
+        do
+        {
+            cout << "Nhap 1 de nhap nhan vien van phong, 2 de nhap nhan vien san xuat: ";
+            cin >> n;
+        }while(n > 2 || n < 1);
+        if(n == 1)
+        {
+            cout << "Nhan vien van phong\n";
+            x[i] = new NVVP;
+            x[i]->Nhap();
+        }
+        else if(n == 2)
+        {
+            cout << "Nhan vien san xuat\n";
+            x[i] = new NVSX;
+            x[i]->Nhap();
+        }
+    }
+
+    cout << "\n===Danh sach nhan vien===\n";
+    for(int i = 0; i < n1; i++)
+    {
+        x[i]->Xuat();
+    }
+
+    cout << "\n===Nhan vien co luong cao nhat, nhan vien co luong thap nhat===\n";
+    int max = 0;
+    int min = 0;
+    for(int i = 0; i < n1; i++)
+    {
+        if(x[i]->Tinh_luong() > x[max]->Tinh_luong())
+        {
+            max = i;
+        }
+        if(x[i]->Tinh_luong() < x[min]->Tinh_luong())
+        {
+            min = i;
+        }
+    }
+    cout << "Nhan vien co luong cao nhat: \n";
+    x[max]->Xuat();
+    cout << "Nhan vien co luong thap nhat: \n";
+    x[min]->Xuat();
+    return 0;
+}
---
+++ bai_2.cpp
@@ -0,0 +1,308 @@
+#include<iostream>
+#include<string.h>
+#include<math.h>
+#include<iomanip>
+#include<algorithm>
+#include<vector>
+using namespace std;
+typedef long long ll;
+
+class Nguoi
+{
+protected:
+    string HoTen;
+    int NamSinh;
+    string DiaChi;
+public:
+    Nguoi();
+    ~Nguoi();
+    virtual void Nhap();
+    virtual void Xuat();
+};
+
+class SinhVien : public Nguoi
+{
+protected:
+    int MaSV;
+    double DiemTB;
+public:
+    SinhVien();
+    ~SinhVien();
+    void Nhap();
+    void Xuat();
+};
+
+class CongNhan : public Nguoi
+{
+protected:
+    int LuongCoBan;
+    int SoNgayPhep;
+    int SoNamKN;
+public:
+    CongNhan();
+    ~CongNhan();
+    void Nhap();
+    void Xuat();
+};
+
+class CaSi :public Nguoi
+{
+protected:
+    string DongNhac;
+    int SoShowDaDien;
+    int SoAlbumRaMat;
+public:
+    CaSi();
+    ~CaSi();
+    void Nhap();
+    void Xuat();
+};
+
+class BacSi :public Nguoi
+{
+protected:
+    string KhoaChuyenMon;
+    int SoNamChuaBenh;
+public:
+    BacSi();
+    ~BacSi();
+    void Nhap();
+    void Xuat();
+};
+
+//Nguoi
+Nguoi ::Nguoi()
+{
+    HoTen = " ";
+    NamSinh = 0;
+    DiaChi = " ";
+}
+
+Nguoi ::~Nguoi()
+{
+    HoTen = " ";
+    NamSinh = 0;
+    DiaChi = " ";
+}
+
+void Nguoi ::Nhap()
+{
+    cout << "Nhap ten: ";
+    cin.ignore();
+    getline(cin, HoTen);
+    cout << "Nhap nam sinh: ";
+    cin >> NamSinh;
+    cout << "Nhap dia chi: ";
+    cin.ignore();
+    getline(cin, DiaChi);
+}
+
+void Nguoi ::Xuat()
+{
+    cout << "+Ten: " << HoTen << endl;
+    cout << "+Nam sinh: " << NamSinh << endl;
+    cout << "+Dia chi: " << DiaChi << endl;
+}
+
+//Sinh vien
+SinhVien ::SinhVien()
+{
+    MaSV = 0;
+    DiemTB = 0;
+}
+
+SinhVien ::~SinhVien()
+{
+    MaSV = 0;
+    DiemTB = 0;
+}
+
+void SinhVien ::Nhap()
+{
+    Nguoi ::Nhap();
+    cout << "Nhap ma sinh vien: ";
+    cin >> MaSV;
+    cout << "Nhap diem trung binh: ";
+    cin >> DiemTB;
+}
+
+void SinhVien ::Xuat()
+{
+    Nguoi ::Xuat();
+    cout << "+Ma sinh vien: " << MaSV << endl;
+    cout << "+Diem trung binh: " << DiemTB << endl;
+}
+
+//Cong nhan
+CongNhan ::CongNhan()
+{
+    LuongCoBan = 0;
+    SoNgayPhep = 0;
+    SoNamKN = 0;
+}
+
+CongNhan ::~CongNhan()
+{
+    LuongCoBan = 0;
+    SoNgayPhep = 0;
+    SoNamKN = 0;
+}
+
+void CongNhan ::Nhap()
+{
+    Nguoi ::Nhap();
+    cout << "Nhap so luong co ban: ";
+    cin >> LuongCoBan;
+    cout << "Nhap so ngay phep: ";
+    cin >> SoNgayPhep;
+    cout << "So nam kinh nghiem: ";
+    cin >> SoNamKN;
+}
+
+void CongNhan ::Xuat()
+{
+    Nguoi ::Xuat();
+    cout << "+Luong co ban: " << LuongCoBan << endl;
+    cout << "+So ngay phep: " << SoNgayPhep << endl;
+    cout << "+So nam kinh nghiem: " << SoNamKN << endl;
+}
+
+//Ca si
+CaSi ::CaSi()
+{
+    DongNhac = "";
+    SoShowDaDien = 0;
+    SoAlbumRaMat = 0;
+}
+
+CaSi ::~CaSi()
+{
+    DongNhac = "";
+    SoShowDaDien = 0;
+    SoAlbumRaMat = 0;
+}
+
+void CaSi ::Nhap()
+{
+    Nguoi ::Nhap();
+    cout << "Nhap dong nhac: ";
+    cin.ignore();
+    getline(cin, DongNhac);
+    cout << "Nhap so show da dien: ";
+    cin >> SoShowDaDien;
+    cout << "Nhap so album ra mat: ";
+    cin >> SoAlbumRaMat;
+}
+
+void CaSi ::Xuat()
+{
+    Nguoi ::Xuat();
+    cout << "+Dong Nhac: " << DongNhac << endl;
+    cout << "+So show da dien: " << SoShowDaDien << endl;
+    cout << "+So Album ra mat: " << SoAlbumRaMat << endl;
+}
+
+//Bac si
+BacSi ::BacSi()
+{
+    KhoaChuyenMon = "";
+    SoNamChuaBenh = 0;
+}
+
+BacSi ::~BacSi()
+{
+    KhoaChuyenMon = "";
+    SoNamChuaBenh = 0;
+}
+
+void BacSi ::Nhap()
+{
+    Nguoi ::Nhap();
+    cout << "Nhap khoa chuyen mon: ";
+    cin.ignore();
+    getline(cin, KhoaChuyenMon);
+    cout << "Nhap so nam chua benh: ";
+    cin >> SoNamChuaBenh;;
+}
+
+void BacSi ::Xuat()
+{
+    Nguoi ::Xuat();
+    cout << "+Khoa chuyen mon: " << KhoaChuyenMon << endl;
+    cout << "+So nam chua benh: " << SoNamChuaBenh << endl;
+}
+
+int main()
+{
+    cout << "Nhap so luong doi tuong muon nhap: ";
+    int n;
+    cin >> n;
+    vector <Nguoi*> a;
+    int n1 = 0, n2 = 0, n3 = 0, n4 = 0;
+    while(n--)
+    {
+        Nguoi *x = NULL;
+        cout << "\t1.Nhap sinh vien\n";
+        cout << "\t2.Nhap cong nhan\n";
+        cout << "\t3.Nhap ca si\n";
+        cout << "\t4.Nhap ba si\n";
+
+        int t;
+        cout << "==>> Nhap lua chon: ";
+        cin >> t;
+
+        switch(t)
+        {
+        case 1:
+        {
+            x = new SinhVien;
+            x->Nhap();
+            a.push_back(x);
+            n1++;
+            break;
+        }
+
+        case 2:
+        {
+            x = new CongNhan;
+            x->Nhap();
+            a.push_back(x);
+            n2++;
+            break;
+        }
+
+        case 3:
+        {
+            x = new CaSi;
+            x->Nhap();
+            a.push_back(x);
+            n3++;
+            break;
+        }
+
+        case 4:
+        {
+            x = new BacSi;
+            x->Nhap();
+            a.push_back(x);
+            n4++;
+            break;
+        }
+        }
+    }
+
+    cout << "\n====Danh sach vua nhap====";
+    for(int i = 0; i < a.size(); i++)
+    {
+        cout << "\n";
+        a[i]->Xuat();
+        cout << "\n-------------------------------\n";
+    }
+
+    cout << "\nTrong danh sach co: ";
+    cout << "\t+" << n1 << "sinh vien\n";
+    cout << "\t+" << n2 << "cong nhan\n";
+    cout << "\t+" << n3 << "ca si\n";
+    cout << "\t+" << n4 << "bac si\n";
+    return 0;
+}
